0749634e3c85a649c1991ce0bcc819691e57d9e0
Alex <alexey.b@qualisystems.com>
2016-10-04 11:45:57 +0300

    Modify vlan creation/removing and add error handling to connectivity operations

--- cloudshell/networking/brocade/fastiron/handler/brocade_fastiron_connectivity_operations.py
+++ cloudshell/networking/brocade/fastiron/handler/brocade_fastiron_connectivity_operations.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import re
+
 from cloudshell.networking.brocade.brocade_connectivity_operations import BrocadeConnectivityOperations
 from cloudshell.networking.networking_utils import validateVlanNumber, validateVlanRange
 
@@ -43,27 +45,31 @@ class BrocadeFastIronConnectivityOperations(BrocadeConnectivityOperations):
                     if start > end:
                         start, end = end, start
                     self.cli_service.send_config_command("vlan {start} to {end}".format(start=start, end=end))
-                    self.cli_service.send_config_command(command="{tag_type} {if_name}".format(tag_type=tag_type,
-                                                                                               if_name=if_name),
-                                                         expected_str=r"vlan\s*{0}|VLAN\s*{0}".format(end))
+                    res = self.cli_service.send_config_command(command="{tag_type} {if_name}".format(tag_type=tag_type,
+                                                                                                     if_name=if_name),
+                                                               expected_str=r"vlan\s*{0}|VLAN\s*{0}".format(end))
+                    if re.search(r"error", res, re.IGNORECASE|re.DOTALL):
+                        raise Exception(self.__class__.__name__, "Error during vlan creation. See logs for details")
                 else:
                     raise Exception(self.__class__.__name__, "Wrong VLAN range declaration'{}'.".format(vlan))
             else:
                 if validateVlanNumber(vlan):
                     self.cli_service.send_config_command("vlan {}".format(vlan))
-                    self.cli_service.send_config_command(command="{tag_type} {if_name}".format(tag_type=tag_type,
-                                                                                               if_name=if_name),
-                                                         expected_str=r"vlan\s*{0}|VLAN\s*{0}".format(vlan))
+                    res = self.cli_service.send_config_command(command="{tag_type} {if_name}".format(tag_type=tag_type,
+                                                                                                     if_name=if_name),
+                                                               expected_str=r"vlan\s*{0}|VLAN\s*{0}".format(vlan))
+                    if re.search(r"error", res, re.IGNORECASE|re.DOTALL):
+                        raise Exception(self.__class__.__name__, "Error during vlan creation. See logs for details")
                 else:
                     raise Exception(self.__class__.__name__, "Wrong VLAN number '{}'.".format(vlan))
 
             self.cli_service.send_config_command("")
-            self.cli_service.exit_configuration_mode()
 
             if qnq and self._does_interface_support_qnq(if_name):
                 self.cli_service.send_config_command("interface {if_name}".format(if_name=if_name))
                 self.cli_service.send_config_command("tag-profile enable")
-                self.cli_service.exit_configuration_mode()
+
+        self.cli_service.send_config_command("end")
 
         return "Vlan Configuration Completed"
 
@@ -79,36 +85,41 @@ class BrocadeFastIronConnectivityOperations(BrocadeConnectivityOperations):
         self.validate_vlan_methods_incoming_parameters(vlan_range, port, port_mode)
         if_name = self.get_port_name(port)
 
-        for vlan in vlan_range.split(","):
-            if port_mode == "trunk":
-                tag_type = "tagged"
-            elif port_mode == "access":
-                tag_type = "untagged"
-            else:
-                raise Exception(self.__class__.__name__,
-                                "Unsupported port mode '{}'. Should be 'trunk' or 'access'".format(port_mode))
+        if port_mode == "trunk":
+            tag_type = "tagged"
+        elif port_mode == "access":
+            tag_type = "untagged"
+        else:
+            raise Exception(self.__class__.__name__,
+                            "Unsupported port mode '{}'. Should be 'trunk' or 'access'".format(port_mode))
 
+        for vlan in vlan_range.split(","):
             if "-" in vlan:
                 if len(vlan.split("-")) == 2 and validateVlanRange(vlan):
                     start, end = map(int, vlan.split("-"))
                     if start > end:
                         start, end = end, start
                     self.cli_service.send_config_command("vlan {start} to {end}".format(start=start, end=end))
-                    self.cli_service.send_config_command(command="no {tag_type} {if_name}".format(tag_type=tag_type,
-                                                                                                  if_name=if_name),
-                                                         expected_str=r"vlan\s*{0}|VLAN\s*{0}".format(end))
+                    res = self.cli_service.send_config_command(command="no {tag_type} {if_name}".format(tag_type=tag_type,
+                                                                                                        if_name=if_name),
+                                                               expected_str=r"vlan\s*{0}|VLAN\s*{0}".format(end))
+                    if re.search(r"error", res, re.IGNORECASE|re.DOTALL):
+                        raise Exception(self.__class__.__name__, "Error during vlan removing. See logs for details")
                 else:
                     raise Exception(self.__class__.__name__, "Wrong VLAN range declaration'{}'.".format(vlan))
             else:
                 if validateVlanNumber(vlan):
                     self.cli_service.send_config_command("vlan {}".format(vlan))
-                    self.cli_service.send_config_command(command="no {tag_type} {if_name}".format(tag_type=tag_type,
-                                                                                                  if_name=if_name),
-                                                         expected_str=r"vlan\s*{0}|VLAN\s*{0}".format(vlan))
+                    res = self.cli_service.send_config_command(command="no {tag_type} {if_name}".format(tag_type=tag_type,
+                                                                                                        if_name=if_name),
+                                                               expected_str=r"vlan\s*{0}|VLAN\s*{0}".format(vlan))
+                    if re.search(r"error", res, re.IGNORECASE | re.DOTALL):
+                        raise Exception(self.__class__.__name__, "Error during vlan removing. See logs for details")
                 else:
                     raise Exception(self.__class__.__name__, "Wrong VLAN number '{}'.".format(vlan))
 
             self.cli_service.send_config_command("")
-            self.cli_service.exit_configuration_mode()
+
+        self.cli_service.send_config_command("end")
 
         return "Remove Vlan Completed"
